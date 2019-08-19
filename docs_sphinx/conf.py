d9f8d6e5c001a143568a751d1a9cd5c9e6fea951
Aleksandra Teska <aleksandra.teska@gmail.com>
2019-08-19 03:29:27 +0200

    add logo

--- docs_sphinx/conf.py
+++ docs_sphinx/conf.py
@@ -80,6 +80,7 @@ if not on_rtd:
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = '_static/brian-logo.png'
 
 
 # -- Extension configuration -------------------------------------------------
