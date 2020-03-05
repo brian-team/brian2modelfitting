Fix tests that still used multiplicative normalization

brian2modelfitting/tests/test_metric.py
@@ -140,7 +140,7 @@ def test_get_features_gamma():
     features = gf.get_features(model_spikes, data_spikes, 0.1*ms)
     assert_equal(np.shape(features), (2, 2))
     assert(np.all(np.array(features) > -1))
-    normed_gf = GammaFactor(delta=0.5 * ms, time=10 * ms, normalization=2.)
+    normed_gf = GammaFactor(delta=0.5 * ms, time=10 * ms, normalization=1/2.)
     normed_features = normed_gf.get_features(model_spikes, data_spikes,
                                              0.1 * ms)
     assert_equal(normed_features, 2*features)

brian2modelfitting/tests/test_modelfitting_tracefitter.py
@@ -281,7 +281,7 @@ def test_fitter_refine_direct(setup):
     # The algorithm is deterministic and should therefore give the same result
     # for the second run
     params, result = tf.refine({'g': 5 * nS}, g=[1 * nS, 30 * nS],
-                               normalization=2)
+                               normalization=1/2)
     assert result.chisqr == 4 * error
 
 