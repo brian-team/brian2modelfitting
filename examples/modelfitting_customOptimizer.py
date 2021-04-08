8889b74693b40e22eba38e7884c96bd9cc034c14
Eslam Khaled <eslamkhaled64@gmail.com>
2021-04-08 04:09:56 +0200

    Update the used model in modelfitting_customOptimizer example

--- examples/modelfitting_customOptimizer.py
+++ examples/modelfitting_customOptimizer.py
@@ -7,6 +7,8 @@ We will use a simple wrapper for Dragonfly optimizers, an open source library fo
 from dragonfly.exd import domains
 from dragonfly.exd.experiment_caller import EuclideanFunctionCaller
 from dragonfly.opt import random_optimiser, gp_bandit, ga_optimiser
+import pandas as pd
+
 from brian2 import *
 from brian2modelfitting import *
 
@@ -69,34 +71,81 @@ class DragonFlyOptimizer(Optimizer):
 # ---------------------------------------------------------------------------- #
 
 
+# helper func.
+def visualize_results(fitter, n_traces):
+    # Show results
+    all_output = fitter.results(format='dataframe')
+    print(all_output)
+
+    # Visualization of the results
+    fits = fitter.generate_traces(params=None, param_init={'v': -65*mV})
+
+    fig, axes = plt.subplots(ncols=n_traces, figsize=(20, 5), sharey=True)
+
+    for ax, data, fit in zip(axes, out_traces, fits):
+        ax.plot(data.transpose())
+        ax.plot(fit.transpose()/mV)
+    plt.show()
+
+
+def load_data(input_data_file='input_traces_hh.csv', output_data_file='output_traces_hh.csv'):
+    # Load Input and Output Data
+    df_inp_traces = pd.read_csv(input_data_file)
+    df_out_traces = pd.read_csv(output_data_file)
+
+    inp_traces = df_inp_traces.to_numpy()
+    inp_traces = inp_traces[:, 1:]
+
+    out_traces = df_out_traces.to_numpy()
+    out_traces = out_traces[:, 1:]
+    return inp_traces, out_traces
+
+
 if __name__ == '__main__':
-    # create input and output
-    input_traces = zeros((10, 5))*volt
-    for i in range(5):
-        input_traces[5:, i] = i*10*mV
+    # Load Input and Output Data
+    inp_traces, out_traces = load_data(
+        input_data_file='input_traces_hh.csv', output_data_file='output_traces_hh.csv')
+    # Parameters
+    area = 20000*umetre**2
+    El = -65*mV
+    EK = -90*mV
+    ENa = 50*mV
+    VT = -63*mV
+    dt = 0.01*ms
+    defaultclock.dt = dt
 
-    output_traces = 10*nS*input_traces
-
-    model = Equations('''
-        I = g*(v-E) : amp
-        g : siemens (constant)
-        E : volt (constant)
-        ''')
+    # Hodgkin-Huxley Model Definition
+    eqs = Equations(
+        '''
+    dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
+    dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/
+        (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
+        (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
+    dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/
+        (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+    dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+    g_na : siemens (constant)
+    g_kd : siemens (constant)
+    gl   : siemens (constant)
+    Cm   : farad (constant)
+    ''')
 
     df_opt = DragonFlyOptimizer(method="bo")
     metric = MSEMetric()
 
-    # passing parameters to the NeuronGroup
-    fitter = TraceFitter(model=model, dt=0.1*ms,
-                         input_var='v', output_var='I',
-                         input=input_traces, output=output_traces,
-                         n_samples=1,)
+    # Fitting
+    fitter = TraceFitter(model=eqs, input_var='I', output_var='v',
+                         input=inp_traces*amp, output=out_traces*mV, dt=dt,
+                         n_samples=1,
+                         param_init={'v': -65*mV},
+                         method='exponential_euler')
 
     res, error = fitter.fit(n_rounds=6,
                             optimizer=df_opt, metric=metric,
-                            g=[1*nS, 30*nS], E=[-20*mV, 100*mV],
                             callback='progressbar',
-                            )
-    results_log = fitter.results(format="dataframe")
-    print(results_log)
-    print(res, error)
+                            gl=[1e-09 * siemens, 1e-07 * siemens],
+                            g_na=[2e-06*siemens, 2e-04*siemens],
+                            g_kd=[6e-07*siemens, 6e-05*siemens],
+                            Cm=[0.1*ufarad*cm**-2 * area, 2*ufarad*cm**-2 * area])
+
+    visualize_results(fitter, out_traces.shape[0])
