88a1a39164d25c63a0668956ad41d35dd952c5a3
antelk <alojic00@fesb.hr>
2021-07-08 17:28:30 +0200

    add small changes due to changes in Inferencer class

--- examples/hh_sbi.py
+++ examples/hh_sbi.py
@@ -14,19 +14,19 @@ out_traces = df_out_traces.to_numpy()
 out_traces = out_traces[:2, 1:]
 
 # Model and its parameters
-area = 20_000 * um ** 2
-El = -65 * mV
-EK = -90 * mV
-ENa = 50 * mV
-VT = -63 * mV
-dt = 0.01 * ms
+area = 20_000*um**2
+El = -65*mV
+EK = -90*mV
+ENa = 50*mV
+VT = -63*mV
+dt = 0.01*ms
 eqs = '''
     dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
     dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/
-        (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
-        (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
+            (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
+            (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
     dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/
-        (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+            (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
     dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
     g_na : siemens (constant)
     g_kd : siemens (constant)
@@ -35,7 +35,7 @@ eqs = '''
 '''
 
 # Time domain
-t = arange(0, out_traces.shape[1] * dt / ms, dt / ms)
+t = arange(0, out_traces.shape[1]*dt/ms, dt/ms)
 
 
 def n_peaks(x):
@@ -48,8 +48,8 @@ def n_peaks(x):
 
 # Simulation-based inference object instantiation
 inferencer = Inferencer(dt=dt, model=eqs,
-                        input={'I': inp_traces * amp},
-                        output={'v': out_traces * mV},
+                        input={'I': inp_traces*amp},
+                        output={'v': out_traces*mV},
                         features=[n_peaks,
                                   lambda x: x[(t > 5) & (t < 10), :].mean(axis=0),
                                   lambda x: x[(t > 5) & (t < 10), :].std(axis=0),
@@ -60,12 +60,12 @@ inferencer = Inferencer(dt=dt, model=eqs,
                         param_init={'v': 'VT'})
 
 # Generate prior and train the neural density estimator
-inferencer.infere(n_samples=1000,
-                  n_rounds=1,
-                  gl=[1e-09 * siemens, 1e-07 * siemens],
-                  g_na=[2e-06 * siemens, 2e-04 * siemens],
-                  g_kd=[6e-07 * siemens, 6e-05 * siemens],
-                  Cm=[0.1 * uF * cm ** -2 * area, 2 * uF * cm ** -2 * area])
+inferencer.infere(n_rounds=2,
+                  n_samples=1000,
+                  gl=[1e-09*siemens, 1e-07*siemens],
+                  g_na=[2e-06*siemens, 2e-04*siemens],
+                  g_kd=[6e-07*siemens, 6e-05*siemens],
+                  Cm=[0.1*uF*cm**-2*area, 2*uF*cm**-2*area])
 
 # Draw samples from posterior
 inferencer.sample((10000,))
@@ -85,8 +85,8 @@ fig, axs = subplots(nrows, ncols, sharex=True,
                     gridspec_kw={'height_ratios': [3, 1]}, figsize=(15, 4))
 for idx in range(ncols):
     axs[0, idx].plot(t, out_traces[idx, :].T, label='measurements')
-    axs[0, idx].plot(t, inf_traces[idx, :].T / mV, label='fits')
-    axs[1, idx].plot(t, inp_traces[idx, :].T / amp, 'k-', label='stimulus')
+    axs[0, idx].plot(t, inf_traces[idx, :].T/mV, label='fits')
+    axs[1, idx].plot(t, inp_traces[idx, :].T/nA, 'k-', label='stimulus')
     axs[1, idx].set_xlabel('t, ms')
     if idx == 0:
         axs[0, idx].set_ylabel('$v$, mV')
